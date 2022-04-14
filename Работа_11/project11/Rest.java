0a447c41fd5c5144f9af5155b1dd5c57675a8515
Inerva <Taksaland@yandex.ru>
2022-04-15 02:51:42 +0300

    11
    11

---
+++ Работа_11/project11/Rest.java
@@ -0,0 +1,13 @@
+package project11;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class Rest {
+    @RequestMapping("/actuator")
+    public @ResponseBody String rest(){
+        return "rest";
+    }
+}
