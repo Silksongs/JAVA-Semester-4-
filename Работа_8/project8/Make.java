7d81f14a692cd7121bdc93bb079a64c5b846a795
Inerva <Taksaland@yandex.ru>
2022-04-15 02:44:11 +0300

    8
    8

---
+++ Работа_8/project8/Make.java
@@ -0,0 +1,10 @@
+package project8;
+
+public class Make {
+    public void make_football_ball(){
+        System.out.println("Сделан футбольный мяч");
+    }
+    public void make_simple_ball(){
+        System.out.println("Сделан обычный мяч");
+    }
+}
---
+++ Работа_8/project8/MakeFootballBall.java
@@ -0,0 +1,12 @@
+package project8;
+
+public class MakeFootballBall implements Command{
+    private Make m;
+    public MakeFootballBall(Make m) {
+        this.m = m;
+    }
+    @Override
+    public void make_ball() {
+        m.make_football_ball();
+    }
+}
